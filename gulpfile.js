39acc04e8b5fc4a001bbdf60314b711835832eea
Oliver LLuberes <olimail47@gmail.com>
2017-12-20 16:40:09 -0400

    feat: sass workflow improved

--- gulpfile.js
+++ gulpfile.js
@@ -3,49 +3,56 @@ const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
+const browser = require('browser-sync').create();
+
 // Testing...
-gulp.task("default", ['copyHtml', 'imageMin', 'sass', 'script']);
+gulp.task("default", ['sass','start']);
+gulp.task("production", ['copyHtml', 'imageMin', 'sass', 'script']);
 
 // Compile HTML Files
 gulp.task("copyHtml", () =>
     gulp.src('src/*.html')
-    .pipe(gulp.dest('public'))
+    .pipe(gulp.dest('dest'))
 );
 
 // Optimize Images
 gulp.task('imageMin', () =>
     gulp.src('src/images/*')
     .pipe(imagemin())
-    .pipe(gulp.dest('public/images'))
+    .pipe(gulp.dest('dest/images'))
 );
 
 // Minify javascript 
-// gulp.task("uglify", () =>
-//     gulp.src('src/js/*.js')
-//         .pipe(uglify())
-//         .pipe(gulp.dest('public/js'))
-// );
+gulp.task("uglify", () =>
+    gulp.src('src/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dest/js'))
+);
 
 // Concatenate scripts
 gulp.task("script", () =>
     gulp.src('src/js/*.js')
     .pipe(concat('main.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('public/js'))
+    .pipe(uglify()) //script task also minifies the js
+    .pipe(gulp.dest('dest/js'))
 );
 
 // Compile sass
-
 gulp.task("sass", () =>
-    gulp.src('src/scss/style.scss')
+    gulp.src('src/scss/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('public/css'))
+    .pipe(gulp.dest('src/css'))
 );
 
 // Watch
-gulp.task('watch', () => {
-    gulp.watch('src/js/*.js', ['script']);
-    gulp.watch('src/scss/style.scss', ['script']);
-    gulp.watch('src/images/*', ['imageMin']);
-    gulp.watch('src/*.html', ['copyHtml']);
+gulp.task('start', () => {
+    browser.init({
+        server: './src'
+    });
+
+    // gulp.watch('src/js/*.js', ['script']);
+    gulp.watch(['src/scss/*.scss'], ['sass']);
+    // gulp.watch(['src/images/*'], ['imageMin']);
+    gulp.watch(['src/*.html']).on('change', browser.reload);
+  
 });
