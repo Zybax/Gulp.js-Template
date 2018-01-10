449f9fd5575cf36fc9f8385e96130ae258d7afcd
Oliver LLuberes <olimail47@gmail.com>
2018-01-10 16:49:09 -0400

    feat: rename plugin clean-css, plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 const gulp = require('gulp');
-const imagemin = require('gulp-imagemin');
+const cleanCSS = require('gulp-clean-css');
+const imageMin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
+const rename = require("gulp-rename");
 const browser = require('browser-sync').create();
 
 // Testing...
-gulp.task("default", ['sass','start']);
-gulp.task("production", ['copyHtml', 'imageMin', 'sass', 'script']);
+gulp.task("default", ['sass', 'start']);
+gulp.task("production", ['copyHtml', 'imageMin', 'sass', 'script', 'minify-css']);
 
 // Compile HTML Files
 gulp.task("copyHtml", () =>
@@ -15,18 +17,33 @@ gulp.task("copyHtml", () =>
     .pipe(gulp.dest('dest'))
 );
 
+// Minify Css
+gulp.task('minify-css', () => {
+    gulp.src('src/css/style.css')
+        .pipe(cleanCSS({
+            debug: true
+        }, (details) => {
+            console.log(`${details.name}: ${details.stats.originalSize}`);
+            console.log(`${details.name}: ${details.stats.minifiedSize}`);
+        }))
+        .pipe(rename({
+            suffix: '.min'
+        })) // Adding suffix min
+        .pipe(gulp.dest('dest/css'));
+});
+
 // Optimize Images
 gulp.task('imageMin', () =>
     gulp.src('src/images/*')
-    .pipe(imagemin())
+    .pipe(imageMin())
     .pipe(gulp.dest('dest/images'))
 );
 
 // Minify javascript 
 gulp.task("uglify", () =>
     gulp.src('src/js/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('dest/js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dest/js'))
 );
 
 // Concatenate scripts
@@ -34,6 +51,9 @@ gulp.task("script", () =>
     gulp.src('src/js/*.js')
     .pipe(concat('main.js'))
     .pipe(uglify()) //script task also minifies the js
+    .pipe(rename({
+        suffix: '.min'
+    })) // Adding suffix min
     .pipe(gulp.dest('dest/js'))
 );
 
@@ -54,5 +74,5 @@ gulp.task('start', () => {
     gulp.watch(['src/scss/*.scss'], ['sass']);
     // gulp.watch(['src/images/*'], ['imageMin']);
     gulp.watch(['src/*.html']).on('change', browser.reload);
-  
+
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,9 @@
 console.log("test");
 
-
+console.log("test");
+console.log("test");
+console.log("test");
+console.log("test");
+console.log("test");
+console.log("test");
 console.log("test");
